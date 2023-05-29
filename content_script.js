2d025b719dd96a8ffd4d651cb30d40038ee63f2d
yizhou <yizhou.chen@grabtaxi.com>
2023-05-30 07:03:19 +0800

    fix clicking button not trigger translate issue

--- content_script.js
+++ content_script.js
@@ -4,80 +4,103 @@ let pathArray = currentURL.pathname.split('/');
 let baseURL = "https://api.swift.gg/content/";
 let url = baseURL + pathArray[pathArray.length-2] + '/' + pathArray[pathArray.length-1];
 
-fetch(url)
+updateAHerfToAbsolutURL()
+fetchRelatedData(url)
+waitPageLoaded()
+
+function waitPageLoaded() {
+    var maxCheckCount = 10
+    var currentCheckCount = 0
+    window.addEventListener('load', () => {
+      const interval = setInterval(() => {
+        if (currentCheckCount >= maxCheckCount) {
+          clearInterval(interval);
+        }
+        currentCheckCount ++;
+        const element = document.querySelector("div.headline h1");
+        if (element) {
+          clearInterval(interval);
+          addTitleNode();
+          appendH2Nodes();
+          appendPNodes();
+        }
+      }, 200);
+    });
+}
+
+function fetchRelatedData(url) {
+    fetch(url)
   .then(response => response.json())
   .then(data => {
     json = data;
-    addTitleNode();
-    appendH2Nodes();
-    appendPNodes();
   });
+}
 
+function updateAHerfToAbsolutURL() {
+  let relativeLinks = document.querySelectorAll('a[href^="/"]');
+  for (let i = 0; i < relativeLinks.length; i++) {
+    let link = relativeLinks[i];
+    let relativePath = link.getAttribute('href');
+    let absolutePath = `https://developer.apple.com${relativePath}`;
+    link.setAttribute('href', absolutePath);
+    link.setAttribute('target', '_blank');
+  }
+}
 
 function addTitleNode() {
-    var title = document.querySelector("div.headline h1");
-    titleText = json[title.innerText.trim()].zh;
-    // console.log(title.innerText.trim());
-    // console.log(titleText);
-    if (!titleText || titleText == "") {
-        return;
-    }
-    var newNode = document.createElement("h3");
-    var text = document.createTextNode(titleText);
-    newNode.appendChild(text);
-    
-    var parent = title.parentElement;
-    parent.insertBefore(newNode, title);
+  var title = document.querySelector("div.headline h1");
+  titleText = json[title.innerText.trim()].zh;
+  if (!titleText || titleText == "") {
+    return;
+  }
+  var newNode = document.createElement("h3");
+  var text = document.createTextNode(titleText);
+  newNode.appendChild(text);
+  var parent = title.parentElement;
+  parent.insertBefore(newNode, title);
 }
 
 function appendH2Nodes() {
-    var h2Nodes = document.querySelectorAll("h2");
+  var h2Nodes = document.querySelectorAll("h2");
 
-    Array.from(h2Nodes).filter((node) => Boolean(json[node.innerText])).forEach((node) => {
-        // debugger;
-        // console.log(json[node.innerText]["zh"]);
-        var parent = node.parentElement;
-
-        var newNode = document.createElement("h2");
-        var t = document.createTextNode(json[node.innerText].zh);
-        newNode.appendChild(t);
-
-        parent.insertBefore(newNode, node);
-        // insertAfter(p, node);
-    })
+  Array.from(h2Nodes).filter((node) => Boolean(json[node.innerText])).forEach((node) => {
+    var parent = node.parentElement;
+    var newNode = document.createElement("h2");
+    var t = document.createTextNode(json[node.innerText].zh);
+    newNode.appendChild(t);
+    parent.insertBefore(newNode, node);
+  })
 }
 
-
 function cloneNode() {
-    var div = document.querySelector("#introduction div.intro div.content");
-    console.log(div.innerHTML); // "title"
-    var cloneNode = div.cloneNode(true);
-    console.log(cloneNode);
-    div.append(cloneNode);
+  var div = document.querySelector("#introduction div.intro div.content");
+  var cloneNode = div.cloneNode(true);
+  div.append(cloneNode);
 }
 
 function appendPNodes() {
-    var pNodes = document.querySelectorAll("p");
-
-    Array.from(pNodes).filter((node) => Boolean(json[node.innerText])).forEach((node) => {
-        // debugger;
-        // console.log(json[node.innerText]["zh"]);
-        var parent = node.parentElement;
-
-        var newNode = document.createElement("p");
-        var t = document.createTextNode(json[node.innerText].zh);
-        newNode.appendChild(t);
-
-        parent.insertBefore(newNode, node);
-        // insertAfter(p, node);
-    })
+  var pNodes = document.querySelectorAll("p");
+  Array.from(pNodes).filter((node) => Boolean(json[node.innerText])).forEach((node) => {
+    var parent = node.parentElement;
+    var newNode = document.createElement("p");
+    var t = document.createTextNode(json[node.innerText].zh);
+    newNode.appendChild(t);
+    parent.insertBefore(newNode, node);
+  })
 }
 
 function insertAfter(newElement, targetElement) {
-    var parent = targetElement.parentElement;
-    if (parent.lastChild == targetElement) {
-        parent.appendChild(newElement);
-    } else {
-        parent.insertBefore(newElement, targetElement.nextSibline);
-    }
+  var parent = targetElement.parentElement;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement, targetElement.nextSibline);
+  }
 }
+
+function delay(i) {
+  setTimeout(() => {
+    console.log(i);
+  }, i * 1000);
+}
+
